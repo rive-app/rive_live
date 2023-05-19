d20d0bd9fbfd2fd97821951a4179485435fbaf4d
Gordon Hayes <pggordonhayes@gmail.com>
2023-05-19 18:30:00 +0200

    cleanup

--- packages/common_repository/lib/src/models/messages.dart
+++ packages/common_repository/lib/src/models/messages.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:common_repository/src/models/exceptions.dart';
+import '../../common_respository.dart';
 
 enum MessageType {
   connect,
